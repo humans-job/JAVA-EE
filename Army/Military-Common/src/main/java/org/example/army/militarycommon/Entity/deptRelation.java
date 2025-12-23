4154b8a92b293da9eed02c496a226b38e671d46a
故里 <528991969@qq.com>
2025-12-24 00:25:32 +0800

    修改

--- Army/Military-Common/src/main/java/org/example/army/militarycommon/Entity/deptRelation.java
+++ Army/Military-Common/src/main/java/org/example/army/militarycommon/Entity/deptRelation.java
@@ -5,7 +5,7 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
-@TableName("biz_dept_belong")
+@TableName("sys_dept_belong")
 public class deptRelation {
     @TableField("id") // 假设id是复合主键的一部分
     private Long childId;// 明确指定主键列名，如果是雪花算法用 ASSIGN_ID
--- Army/Military-Common/src/main/java/org/example/army/militarycommon/Entity/militiaInfo.java
+++ Army/Military-Common/src/main/java/org/example/army/militarycommon/Entity/militiaInfo.java
@@ -9,7 +9,7 @@ import lombok.Data;
 import java.util.Date;
 
 @Data
-@TableName("biz_notice")
+@TableName("biz_militia_info")
 public class militiaInfo  {
     @TableId(value = "id", type = IdType.AUTO) // 明确指定主键列名，如果是雪花算法用 ASSIGN_ID
     private Long id;
