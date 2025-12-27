390bdeb4ef41e95098a763bcedd2e3150691db41
故里 <528991969@qq.com>
2025-12-28 00:39:21 +0800

    配置更新

---
+++ Army/Military-Map/src/main/java/edu/MapApplication.java
@@ -0,0 +1,16 @@
+package edu;
+
+import org.example.army.militarycommon.config.MybatisPlusConfig;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Import(MybatisPlusConfig.class)
+@MapperScan(basePackages = "org.example.army.militarycommon.mapper")
+public class MapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MapApplication.class, args);
+    }
+}
